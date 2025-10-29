8809bb2764bcc6c05071aa4ead64ae3ca6917244
Alexandr Khakana <Kalashiq.org@gmail.com>
2025-10-29 08:30:55 +0200

    fix: fix bug with imports

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,5 @@ export { AnimatedButton } from './AnimatedButton';
 export { Header } from './Header';
 export { PageHeader } from './PageHeader';
 export { StatsBlock } from './StatsBlock';
-export { PeriodTabs } from './PeriodTabs';
+// PeriodTabs directory lives under AnimatedButton/PeriodTabs
+export { PeriodTabs } from './AnimatedButton/PeriodTabs';
